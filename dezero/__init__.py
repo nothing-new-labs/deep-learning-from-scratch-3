a00cc4604f5280719d79ca2b6332832590e28f66
DavidLi <davidli2010@foxmail.com>
2025-12-08 21:17:54 +0800

    stage 2

---
+++ dezero/__init__.py
@@ -0,0 +1,9 @@
+is_simple_core = True
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
